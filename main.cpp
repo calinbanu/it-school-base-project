dbcb290524c5cf3e3cfb8f5b32db46afed3a8803
Banu Calin <banucaliniulian@gmail.com>
2026-03-04 18:02:57 +0000

    New message

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 int main(int, char **)
 {
-    std::cout << "Hello, from new branch in cpp-dev!\n";
+    std::cout << "Hello, from new brench in cpp-dev!\n";
     std::cout << "2 + 3 = " << add(2, 3) << "\n";
     std::cout << "5! = " << factorial(5) << "\n";
 }
